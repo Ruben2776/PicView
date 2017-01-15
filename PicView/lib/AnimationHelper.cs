b83f12205e9990c9af221640bfe9ba5d312917b5
Ruben2776 <ruben@live.dk>
2017-01-15 04:11:42 +0100

    Renaming project

---
+++ PicView/lib/AnimationHelper.cs
@@ -0,0 +1,490 @@
+﻿using PicView.Properties;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace PicView.lib
+{
+    internal static class AnimationHelper
+    {
+        //private static DoubleAnimation da = new DoubleAnimation();
+        private static readonly ColorAnimation ccAnim = new ColorAnimation { Duration = TimeSpan.FromSeconds(.35) };
+
+        #region Fade
+        internal static void Fade(UIElement element, double to, Duration duration)
+        {
+            var da = new DoubleAnimation()
+            {
+                To = to,
+                Duration = duration
+            };
+            element.BeginAnimation(UIElement.OpacityProperty, da);
+        }
+
+        internal static void Fade(UIElement element, Duration duration, TimeSpan begintime, double from, double to)
+        {
+            var da = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                BeginTime = begintime,
+                Duration = duration
+            };
+            element.BeginAnimation(UIElement.OpacityProperty, da);
+        }
+
+        //internal static void FadeWindow(Window window, Double to, Duration duration)
+        //{
+        //    var anim = new DoubleAnimation(to, duration);
+        //    anim.Completed += (s, _) => Close(window);
+        //    window.BeginAnimation(UIElement.OpacityProperty, anim);
+        //}
+        #endregion
+
+        #region Color Events
+
+        internal static void MouseLeaveColorEvent(byte a, byte r, byte g, byte b, Brush brush, bool alpha)
+        {
+            ccAnim.From = !alpha ? GetPrefferedColorOver() : GetPrefferedColorOverAlpha();
+            ccAnim.To = Color.FromArgb(a, r, g, b);
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, ccAnim);
+        }
+
+        internal static void MouseEnterColorEvent(byte a, byte r, byte g, byte b, Brush brush, bool alpha)
+        {
+            ccAnim.From = Color.FromArgb(a, r, g, b);
+            ccAnim.To = !alpha ? GetPrefferedColorOver() : GetPrefferedColorOverAlpha();
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, ccAnim);
+        }
+
+        internal static void PreviewMouseLeftButtonDownColorEvent(Brush brush, bool alpha)
+        {
+            if (!alpha)
+            {
+                ccAnim.From = GetPrefferedColorOver();
+                ccAnim.To = GetPrefferedColorDown();
+            }
+            else
+            {
+                ccAnim.From = GetPrefferedColorOverAlpha();
+                ccAnim.To = GetPrefferedColorDownAlpha();
+            }
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, ccAnim);
+        }
+
+        internal static void MouseLeaveColorEvent(byte a, byte r, byte g, byte b, Brush brush, short colortheme)
+        {
+            switch (colortheme)
+            {
+                case 1:
+                default:
+                    ccAnim.From = Settings.Default.Blue;
+                    break;
+                case 2:
+                    ccAnim.From = Settings.Default.Pink;
+                    break;
+                case 3:
+                    ccAnim.From = Settings.Default.Orange;
+                    break;
+                case 4:
+                    ccAnim.From = Settings.Default.Green;
+                    break;
+                case 5:
+                    ccAnim.From = Settings.Default.Red;
+                    break;
+                case 6:
+                    ccAnim.From = Settings.Default.Teal;
+                    break;
+                case 7:
+                    ccAnim.From = Settings.Default.Aqua;
+                    break;
+                case 8:
+                    ccAnim.From = Settings.Default.Sandy;
+                    break;
+                case 9:
+                    ccAnim.From = Settings.Default.Purple;
+                    break;
+                case 10:
+                    ccAnim.From = Settings.Default.Cyan;
+                    break;
+                case 11:
+                    ccAnim.From = Settings.Default.Magenta;
+                    break;
+                case 12:
+                    ccAnim.From = Settings.Default.Yellow;
+                    break;
+            }
+            ccAnim.To = Color.FromArgb(a, r, g, b);
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, ccAnim);
+        }
+
+        internal static void MouseEnterColorEvent(byte a, byte r, byte g, byte b, Brush brush, short colortheme)
+        {
+            ccAnim.From = Color.FromArgb(a, r, g, b);
+            switch (colortheme)
+            {
+                case 1:
+                default:
+                    ccAnim.To = Settings.Default.Blue;
+                    break;
+                case 2:
+                    ccAnim.To = Settings.Default.Pink;
+                    break;
+                case 3:
+                    ccAnim.To = Settings.Default.Orange;
+                    break;
+                case 4:
+                    ccAnim.To = Settings.Default.Green;
+                    break;
+                case 5:
+                    ccAnim.To = Settings.Default.Red;
+                    break;
+                case 6:
+                    ccAnim.To = Settings.Default.Teal;
+                    break;
+                case 7:
+                    ccAnim.To = Settings.Default.Aqua;
+                    break;
+                case 8:
+                    ccAnim.To = Settings.Default.Sandy;
+                    break;
+                case 9:
+                    ccAnim.To = Settings.Default.Purple;
+                    break;
+                case 10:
+                    ccAnim.To = Settings.Default.Cyan;
+                    break;
+                case 11:
+                    ccAnim.To = Settings.Default.Magenta;
+                    break;
+                case 12:
+                    ccAnim.To = Settings.Default.Yellow;
+                    break;
+            }
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, ccAnim);
+        }
+
+        internal static void PreviewMouseLeftButtonDownColorEvent(Brush brush, short colortheme)
+        {
+            switch (colortheme)
+            {
+                case 1:
+                default:
+                    ccAnim.From = Settings.Default.Blue;
+                    ccAnim.To = Settings.Default.BlueMouse;
+                    break;
+                case 2:
+                    ccAnim.From = Settings.Default.Pink;
+                    ccAnim.To = Settings.Default.PinkMouse;
+                    break;
+                case 3:
+                    ccAnim.From = Settings.Default.Orange;
+                    ccAnim.To = Settings.Default.OrangeMouse;
+                    break;
+                case 4:
+                    ccAnim.From = Settings.Default.Green;
+                    ccAnim.To = Settings.Default.GreenMouse;
+                    break;
+                case 5:
+                    ccAnim.From = Settings.Default.Red;
+                    ccAnim.To = Settings.Default.RedMouse;
+                    break;
+                case 6:
+                    ccAnim.From = Settings.Default.Teal;
+                    ccAnim.To = Settings.Default.TealMouse;
+                    break;
+                case 7:
+                    ccAnim.From = Settings.Default.Aqua;
+                    ccAnim.To = Settings.Default.AquaMouse;
+                    break;
+                case 8:
+                    ccAnim.From = Settings.Default.Sandy;
+                    ccAnim.To = Settings.Default.SandyMouse;
+                    break;
+                case 9:
+                    ccAnim.From = Settings.Default.Purple;
+                    ccAnim.To = Settings.Default.PurpleMouse;
+                    break;
+                case 10:
+                    ccAnim.From = Settings.Default.Cyan;
+                    ccAnim.To = Settings.Default.CyanMouse;
+                    break;
+                case 11:
+                    ccAnim.From = Settings.Default.Magenta;
+                    ccAnim.To = Settings.Default.MagentaMouse;
+                    break;
+                case 12:
+                    ccAnim.From = Settings.Default.Yellow;
+                    ccAnim.To = Settings.Default.YellowMouse;
+                    break;
+            }
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, ccAnim);
+        }
+
+        #endregion
+
+        #region Color stuff
+
+        #region Return Color
+
+        internal static Color GetPrefferedColorOver()
+        {
+            switch (Settings.Default.ColorTheme)
+            {
+                case 1:
+                default:
+                    return Settings.Default.Blue;
+                case 2:
+                    return Settings.Default.Pink;
+                case 3:
+                    return Settings.Default.Orange;
+                case 4:
+                    return Settings.Default.Green;
+                case 5:
+                    return Settings.Default.Red;
+                case 6:
+                    return Settings.Default.Teal;
+                case 7:
+                    return Settings.Default.Aqua;
+                case 8:
+                    return Settings.Default.Sandy;
+                case 9:
+                    return Settings.Default.Purple;
+                case 10:
+                    return Settings.Default.Cyan;
+                case 11:
+                    return Settings.Default.Magenta;
+                case 12:
+                    return Settings.Default.Yellow;
+
+            }
+        }
+
+        internal static Color GetPrefferedColorDown()
+        {
+            switch (Settings.Default.ColorTheme)
+            {
+                case 1:
+                default:
+                    return Settings.Default.BlueMouse;
+                case 2:
+                    return Settings.Default.PinkMouse;
+                case 3:
+                    return Settings.Default.OrangeMouse;
+                case 4:
+                    return Settings.Default.GreenMouse;
+                case 5:
+                    return Settings.Default.RedMouse;
+                case 6:
+                    return Settings.Default.TealMouse;
+                case 7:
+                    return Settings.Default.AquaMouse;
+                case 8:
+                    return Settings.Default.SandyMouse;
+                case 9:
+                    return Settings.Default.PurpleMouse;
+                case 10:
+                    return Settings.Default.CyanMouse;
+                case 11:
+                    return Settings.Default.MagentaMouse;
+                case 12:
+                    return Settings.Default.YellowMouse;
+            }
+        }
+
+        #region Alpha
+
+        internal static Color GetPrefferedColorDownAlpha()
+        {
+            switch (Settings.Default.ColorTheme)
+            {
+                case 1:
+                default:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.BlueMouse.R,
+                        Settings.Default.BlueMouse.G,
+                        Settings.Default.BlueMouse.B);
+                case 2:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.PinkMouse.R,
+                        Settings.Default.PinkMouse.G,
+                        Settings.Default.PinkMouse.B);
+                case 3:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.OrangeMouse.R,
+                        Settings.Default.OrangeMouse.G,
+                        Settings.Default.OrangeMouse.B);
+                case 4:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.GreenMouse.R,
+                        Settings.Default.GreenMouse.G,
+                        Settings.Default.GreenMouse.B);
+                case 5:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.RedMouse.R,
+                        Settings.Default.RedMouse.G,
+                        Settings.Default.RedMouse.B);
+                case 6:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.TealMouse.R,
+                        Settings.Default.TealMouse.G,
+                        Settings.Default.TealMouse.B);
+                case 7:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.AquaMouse.R,
+                        Settings.Default.AquaMouse.G,
+                        Settings.Default.AquaMouse.B);
+                case 8:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.SandyMouse.R,
+                        Settings.Default.SandyMouse.G,
+                        Settings.Default.SandyMouse.B);
+                case 9:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.PurpleMouse.R,
+                        Settings.Default.PurpleMouse.G,
+                        Settings.Default.PurpleMouse.B);
+                case 10:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.CyanMouse.R,
+                        Settings.Default.CyanMouse.G,
+                        Settings.Default.CyanMouse.B);
+                case 11:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.MagentaMouse.R,
+                        Settings.Default.MagentaMouse.G,
+                        Settings.Default.MagentaMouse.B);
+                case 12:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.YellowMouse.R,
+                        Settings.Default.YellowMouse.G,
+                        Settings.Default.YellowMouse.B);
+            }
+        }
+
+        internal static Color GetPrefferedColorOverAlpha()
+        {
+            switch (Settings.Default.ColorTheme)
+            {
+                case 1:
+                default:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Blue.R,
+                        Settings.Default.Blue.G,
+                        Settings.Default.Blue.B);
+                case 2:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Pink.R,
+                        Settings.Default.Pink.G,
+                        Settings.Default.Pink.B);
+                case 3:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Orange.R,
+                        Settings.Default.Orange.G,
+                        Settings.Default.Orange.B);
+                case 4:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Green.R,
+                        Settings.Default.Green.G,
+                        Settings.Default.Green.B);
+                case 5:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Red.R,
+                        Settings.Default.Red.G,
+                        Settings.Default.Red.B);
+                case 6:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Teal.R,
+                        Settings.Default.Teal.G,
+                        Settings.Default.Teal.B);
+                case 7:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Aqua.R,
+                        Settings.Default.Aqua.G,
+                        Settings.Default.Aqua.B);
+                case 8:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Sandy.R,
+                        Settings.Default.Sandy.G,
+                        Settings.Default.Sandy.B);
+                case 9:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Purple.R,
+                        Settings.Default.Purple.G,
+                        Settings.Default.Purple.B);
+                case 10:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Cyan.R,
+                        Settings.Default.Cyan.G,
+                        Settings.Default.Cyan.B);
+                case 11:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Magenta.R,
+                        Settings.Default.Magenta.G,
+                        Settings.Default.Magenta.B);
+                case 12:
+                    return Color.FromArgb(
+                        220,
+                        Settings.Default.Yellow.R,
+                        Settings.Default.Yellow.G,
+                        Settings.Default.Yellow.B);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #region Size Animation
+
+        //internal static void HoverSizeAnim(UIElement element, bool unHover)
+        //{
+        //    var da = new DoubleAnimation();
+        //    if (unHover)
+        //    {
+        //        da.From = 95;
+        //        da.To = 65;
+        //        da.Duration = TimeSpan.FromSeconds(.3);
+        //    }
+        //    else
+        //    {
+        //        da.From = 65;
+        //        da.To = 95;
+        //        da.Duration = TimeSpan.FromSeconds(.25);
+        //    }
+        //    da.AccelerationRatio = 0.4;
+        //    da.DecelerationRatio = 0.6;
+
+        //    element.BeginAnimation(Rectangle.HeightProperty, da);
+        //}
+
+        #endregion
+
+    }
+}
---
+++ PicView/lib/ImageManager.cs
@@ -0,0 +1,158 @@
+﻿using ImageMagick;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace PicView.lib
+{
+    internal static class ImageManager
+    {
+        #region RenderToBitmapSource
+
+        internal static BitmapSource RenderToBitmapSource(string file, string extension)
+        {
+            if (string.IsNullOrWhiteSpace(file) || file.Length < 2)
+                return null;
+
+            if (extension == ".gif")
+                return GetBitmapImage(new Uri(file));
+
+            BitmapSource pic;
+
+            using (MagickImage magick = new MagickImage(file))
+            {                    
+                magick.Quality = 100;
+
+                if (extension == ".svg")
+                {
+                    //Create settings and make background transparent
+                    var mrs = new MagickReadSettings();
+                    mrs.Format = MagickFormat.Svg;
+                    mrs.ColorSpace = ColorSpace.Transparent;
+                    mrs.BackgroundColor = MagickColors.Transparent;
+
+                    //Write to stream as png
+                    magick.Read(file, mrs);
+                    magick.Format = MagickFormat.Png;
+                    var stream = new MemoryStream();
+                    magick.Write(stream);
+                    pic = GetBitmapImage(stream);
+                }
+                else
+                {
+                    magick.Read(file);
+                    magick.ColorSpace = ColorSpace.Transparent;
+                    pic = magick.ToBitmapSource();
+                }
+                    
+                pic.Freeze();
+                return pic;
+            }
+
+        }
+
+        #endregion
+
+        #region GetBitmapImage
+
+        internal static BitmapImage GetBitmapImage(Uri s)
+        {
+            var pic = new BitmapImage();
+            pic.BeginInit();
+            pic.UriSource = s;
+            pic.CacheOption = BitmapCacheOption.None;
+            try
+            {
+                pic.EndInit();
+            }
+            catch (ArgumentException)
+            {
+                var failpic = new BitmapImage();
+                failpic.BeginInit();
+                failpic.UriSource = s;
+                failpic.CacheOption = BitmapCacheOption.None;
+                failpic.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                failpic.CreateOptions = BitmapCreateOptions.IgnoreColorProfile;
+                /// Some images crash without theese settings :(
+                failpic.EndInit();
+                failpic.Freeze();
+                return failpic;
+            }
+            pic.Freeze();
+            return pic;
+        }
+
+        internal static BitmapImage GetBitmapImage(Stream s)
+        {
+            var pic = new BitmapImage();
+            pic.BeginInit();
+            pic.StreamSource = s;
+            pic.CacheOption = BitmapCacheOption.OnLoad;
+            try
+            {
+                pic.EndInit();
+            }
+            catch (ArgumentException)
+            {
+                var failpic = new BitmapImage();
+                failpic.BeginInit();
+                failpic.StreamSource = s;
+                failpic.CacheOption = BitmapCacheOption.OnLoad;
+                failpic.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                failpic.CreateOptions = BitmapCreateOptions.IgnoreColorProfile;
+                /// Some images crash without theese settings :(
+                failpic.EndInit();
+                failpic.Freeze();
+                return failpic;
+            }
+            pic.Freeze();
+            return pic;
+        }
+
+        #endregion
+
+        internal static BitmapSource GetMagickImage(Stream s)
+        {
+            BitmapSource pic;
+            try
+            {
+                using (var magick = new MagickImage())
+                {
+                    magick.Read(s);
+                    magick.Quality = 100;
+                    pic = magick.ToBitmapSource();
+                }
+                pic.Freeze();
+                return pic;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        //internal static BitmapSource GetMagickImage(String s, int width, int height)
+        //{
+        //    BitmapSource pic;
+        //    try
+        //    {
+        //        using (var magick = new MagickImage())
+        //        {
+        //            magick.Read(s);
+        //            magick.Quality = 60;
+        //            magick.AdaptiveResize(width, height);
+        //            pic = magick.ToBitmapSource();
+        //        }
+        //        pic.Freeze();
+        //        return pic;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return null;
+        //    }
+        //}
+
+    }
+}
