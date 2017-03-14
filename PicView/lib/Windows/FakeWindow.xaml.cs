93c506c31a3e8eb537a8b6e1fad0359e4004b8ef
Ruben <ruben@live.dk>
2017-03-14 23:52:00 +0100

    Picture gallery update

---
+++ PicView/lib/Windows/FakeWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PicView.Windows
+{
+    /// <summary>
+    /// Interaction logic for FakeWindow.xaml
+    /// </summary>
+    public partial class FakeWindow : Window
+    {
+        public FakeWindow()
+        {
+            InitializeComponent();
+            Width = SystemParameters.FullPrimaryScreenWidth;
+            Height = SystemParameters.WorkArea.Height;
+        }
+    }
+}
