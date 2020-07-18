b9954b0eb10eb935e0ed894420fb76bb3d21c61a
Ruben <ruben@live.dk>
2020-07-19 00:46:55 +0200

    Update coding

--- PicView/Views/UserControls/Buttons/FlipButton.xaml.cs
+++ PicView/Views/UserControls/Buttons/FlipButton.xaml.cs
@@ -37,20 +37,17 @@ namespace PicView.UILogic.UserControls
                     TransformImage.Rotation.Flip();
                 };
 
-                TheButton.Checked += FlipButton_Checked;
-                TheButton.Unchecked += FlipButton_Unchecked;
+                // Change FlipButton's icon when (un)checked
+                TheButton.Checked += delegate 
+                {
+                    FlipPath.Data = Geometry.Parse("M448,192l-128,96v-64H128v128h248c4.4,0,8,3.6,8,8v48c0,4.4-3.6,8-8,8H72c-4.4,0-8-3.6-8-8V168c0-4.4,3.6-8,8-8h248V96 L448, 192z");
+                };
+
+                TheButton.Unchecked += delegate
+                {
+                    FlipPath.Data = Geometry.Parse("M192,96v64h248c4.4,0,8,3.6,8,8v240c0,4.4-3.6,8-8,8H136c-4.4,0-8-3.6-8-8v-48c0-4.4,3.6-8,8-8h248V224H192v64L64,192 L192, 96z");
+                };
             };
-        }
-
-        // Change FlipButton's icon when (un)checked
-        private void FlipButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            FlipPath.Data = Geometry.Parse("M192,96v64h248c4.4,0,8,3.6,8,8v240c0,4.4-3.6,8-8,8H136c-4.4,0-8-3.6-8-8v-48c0-4.4,3.6-8,8-8h248V224H192v64L64,192 L192, 96z");
-        }
-
-        private void FlipButton_Checked(object sender, RoutedEventArgs e)
-        {
-            FlipPath.Data = Geometry.Parse("M448,192l-128,96v-64H128v128h248c4.4,0,8,3.6,8,8v48c0,4.4-3.6,8-8,8H72c-4.4,0-8-3.6-8-8V168c0-4.4,3.6-8,8-8h248V96 L448, 192z");
         }
     }
 }
