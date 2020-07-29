d2dba9522a667ccac1ddbe0d2cf39418ad1af338
Ruben <ruben@live.dk>
2020-07-29 23:21:47 +0200

    Support spaces in file path when restarting

--- PicView/ConfigureSettings/GeneralSettings.cs
+++ PicView/ConfigureSettings/GeneralSettings.cs
@@ -25,7 +25,21 @@ namespace PicView.ConfigureSettings
                 GetAppPath = GetAppPath.Replace(".dll", ".exe", System.StringComparison.InvariantCultureIgnoreCase);
             }
 
-            Process.Start(new ProcessStartInfo(GetAppPath, Navigation.Pics[Navigation.FolderIndex]));
+            string args;
+            if (Navigation.Pics.Count > Navigation.FolderIndex)
+            {
+                args = Navigation.Pics[Navigation.FolderIndex];
+
+                // Add double qoutations to support file paths with spaces
+                args = args.Insert(0, @"""");
+                args = args.Insert(args.Length - 1, @"""");
+            }
+            else
+            {
+                args = string.Empty;
+            }
+
+            Process.Start(new ProcessStartInfo(GetAppPath, args));
             Application.Current.Shutdown();
         }
     }
