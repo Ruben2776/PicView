ebaecd7dbeaf8fea97c5bc59f0e29f7aa66bb605
Ruben <ruben@live.dk>
2020-08-18 14:39:59 +0200

    misc

--- PicView/Properties/Settings.Designer.cs
+++ PicView/Properties/Settings.Designer.cs
@@ -277,7 +277,7 @@ namespace PicView.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool AutoFitWindow {
             get {
                 return ((bool)(this["AutoFitWindow"]));
