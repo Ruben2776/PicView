fbd0f8fc7cf03676e10edfd2f95f20d63b3257f1
Ruben <ruben@live.dk>
2021-10-22 18:13:44 +0200

    Change logo and icon, work in progress

---
+++ PicView/Views/UserControls/Misc/Icon.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PicView.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Logo.xaml
+    /// </summary>
+    public partial class Icon : UserControl
+    {
+        public Icon()
+        {
+            InitializeComponent();
+        }
+    }
+}
