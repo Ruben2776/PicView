fd0d39bf90b48130946d4228da38340b20c7cb2c
Ruben <ruben@live.dk>
2021-10-27 19:16:41 +0200

    Don't remove values if not needed

--- PicView/ChangeImage/Preloader.cs
+++ PicView/ChangeImage/Preloader.cs
@@ -218,7 +218,7 @@ namespace PicView.ChangeImage
                 //Clean up infront
                 for (int i = (index + 1) + LoadBehind; i < ((index + 1) + LoadInfront * 2); i++)
                 {
-                    if (Pics.Count == 0) { return; }
+                    if (Pics.Count == 0 || Sources.Count < LoadBehind + LoadInfront + 2) { return; }
                     Remove(i % Pics.Count);
                 }
             }
@@ -241,7 +241,7 @@ namespace PicView.ChangeImage
                 //Clean up behind
                 for (int i = index - LoadInfront * 2; i <= endPoint; i++)
                 {
-                    if (Pics.Count == 0) { return; }
+                    if (Pics.Count == 0 || Sources.Count < LoadBehind + LoadInfront + 2) { return; }
                     Remove(i % Pics.Count);
                 }
             }
