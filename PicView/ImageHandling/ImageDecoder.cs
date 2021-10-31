eab2c6634c5dedfa1e95d4e10c43edafa127bfbb
Ruben <ruben@live.dk>
2021-10-31 20:33:14 +0100

    Fix TGA rendered upside down #22

--- PicView/ImageHandling/ImageDecoder.cs
+++ PicView/ImageHandling/ImageDecoder.cs
@@ -23,7 +23,6 @@ namespace PicView.ImageHandling
             if (file == null) { return null; }
             if (file.Length <= 0) { return null; }
 
-            FileStream? filestream = null; // https://devblogs.microsoft.com/dotnet/file-io-improvements-in-dotnet-6/
             switch (file.Extension)
             {
                 case ".jpg":
@@ -36,108 +35,22 @@ namespace PicView.ImageHandling
                 case ".jfif":
                 case ".webp":
                 case ".wbmp":
-                    try
-                    {
-                        filestream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
-                    }
-                    catch (Exception e)
-                    {
-#if DEBUG
-                        Trace.WriteLine("RenderToBitmapSource Skia returned " + file + " null, \n" + e.Message);
-#endif
-                        return null;
-                    }
-                    using (var imgStream = new SKManagedStream(filestream))
-                    {
-                        using var skData = SKData.Create(filestream);
-                        await filestream.DisposeAsync().ConfigureAwait(false);
-                        using var codec = SKCodec.Create(skData);
-                        if (codec is null) { return null; }
+                    return await GetWriteableBitmap(file).ConfigureAwait(false);
 
-                        var sKBitmap = SKBitmap.Decode(codec);
-                        if (sKBitmap is null) { return null; }
-
-                        var skPic = sKBitmap.ToWriteableBitmap();
-                        skPic.Freeze();
-                        sKBitmap.Dispose();
-                        return skPic;
-                    }
+                case ".tga": // TODO some tga files are created upside down https://github.com/Ruben2776/PicView/issues/22
+                    return getDefaultBitmapSource(file, true);
 
                 case ".svg":
                 case ".svgz": // TODO convert to drawingimage instead
-
                 case ".tif":
                 case ".tiff":
                 case ".dds":
-                case "tga": // TODO some tga files are created upside down https://github.com/Ruben2776/PicView/issues/22
                 case ".psd":
                 case ".psb":
                 case ".xcf":
-
-                    MagickImage magickImage = new()
-                    {
-                        Quality = 100,
-                        ColorSpace = ColorSpace.Transparent
-                    };
-                    if (file.Length >= 2147483647) // Streams with a length larger than 2147483647 are not supported, read from file instead
-                    {
-                        try
-                        {
-                            magickImage.Read(file);
-                        }
-                        catch (Exception e)
-                        {
-#if DEBUG
-                            Trace.WriteLine("RenderToBitmapSource MagickImage returned " + file + " null, \n" + e.Message);
-#endif
-                            return null;
-                        }
-                    }
-                    else
-                    {
-                        try
-                        {
-                            filestream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
-                            magickImage.Read(filestream);
-                        }
-                        catch (Exception e)
-                        {
-#if DEBUG
-                            Trace.WriteLine("RenderToBitmapSource MagickImage returned " + file + " null, \n" + e.Message);
-#endif
-                            return null;
-                        }
-
-                        await filestream.DisposeAsync().ConfigureAwait(false);
-                    }
-
-                    var bitmap = magickImage.ToBitmapSource();
-                    magickImage.Dispose();
-                    bitmap.Freeze();
-
-                    return bitmap;
-
+                    return await getMagicBitmapsource(file).ConfigureAwait(false);
                 default: // some formats cause exceptions when using filestream, so defaulting to reading from file
-                    var magick = new MagickImage();
-                    try
-                    {
-                        magick.Read(file);
-                    }
-                    catch (Exception e)
-                    {
-#if DEBUG
-                        Trace.WriteLine("RenderToBitmapSource MagickImage returned " + file + " null, \n" + e.Message);
-#endif
-                        return null;
-                    }
-
-                    magick.Quality = 100;
-
-                    var pic = magick.ToBitmapSource();
-                    magick.Dispose();
-                    pic.Freeze();
-
-                    return pic;
+                    return getDefaultBitmapSource(file);
             }
         }
 
@@ -205,5 +118,113 @@ namespace PicView.ImageHandling
             }
             catch (Exception) { return null; }
         }
+
+        #region Private functions
+
+        private static async Task<WriteableBitmap?> GetWriteableBitmap(FileInfo fileInfo)
+        {
+            FileStream? filestream = null; // https://devblogs.microsoft.com/dotnet/file-io-improvements-in-dotnet-6/
+            byte[] data;
+
+            try
+            {
+                filestream = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+                data = new byte[filestream.Length];
+                await filestream.ReadAsync(data.AsMemory(0, (int)filestream.Length)).ConfigureAwait(false);
+                await filestream.DisposeAsync().ConfigureAwait(false);
+
+                var sKBitmap = SKBitmap.Decode(data);
+                if (sKBitmap is null) { return null; }
+
+                var skPic = sKBitmap.ToWriteableBitmap();
+                skPic.Freeze();
+                sKBitmap.Dispose();
+                return skPic;
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Trace.WriteLine("RenderToBitmapSource Skia returned " + fileInfo + " null, \n" + e.Message);
+#endif
+                return null;
+            }
+        }
+
+        private static async Task<BitmapSource?> getMagicBitmapsource(FileInfo fileInfo)
+        {
+            FileStream? filestream = null;
+            MagickImage magickImage = new()
+            {
+                Quality = 100,
+                ColorSpace = ColorSpace.Transparent
+            };
+            if (fileInfo.Length >= 2147483647) // Streams with a length larger than 2147483647 are not supported, read from file instead
+            {
+                try
+                {
+                    magickImage.Read(fileInfo);
+                }
+                catch (Exception e)
+                {
+#if DEBUG
+                    Trace.WriteLine("RenderToBitmapSource MagickImage returned " + fileInfo.FullName + " null, \n" + e.Message);
+#endif
+                    return null;
+                }
+            }
+            else
+            {
+                try
+                {
+                    filestream = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+                    magickImage.Read(filestream);
+                }
+                catch (Exception e)
+                {
+#if DEBUG
+                    Trace.WriteLine("RenderToBitmapSource MagickImage returned " + fileInfo + " null, \n" + e.Message);
+#endif
+                    return null;
+                }
+
+                await filestream.DisposeAsync().ConfigureAwait(false);
+            }
+
+            var bitmap = magickImage.ToBitmapSource();
+            magickImage.Dispose();
+            bitmap.Freeze();
+
+            return bitmap;
+        }
+
+        private static BitmapSource? getDefaultBitmapSource(FileInfo fileInfo, bool autoOrient = false)
+        {
+            var magick = new MagickImage();
+            try
+            {
+                magick.Read(fileInfo);
+                if (autoOrient)
+                {
+                    magick.AutoOrient();
+                }
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Trace.WriteLine("RenderToBitmapSource MagickImage returned " + fileInfo.Name + " null, \n" + e.Message);
+#endif
+                return null;
+            }
+
+            magick.Quality = 100;
+
+            var pic = magick.ToBitmapSource();
+            magick.Dispose();
+            pic.Freeze();
+
+            return pic;
+        }
+
+        #endregion
     }
 }
