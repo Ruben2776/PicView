f1e2b9fd84e4cb5b0d69552b80df7330a302eeb3
Ruben <ruben@live.dk>
2021-11-21 17:53:18 +0100

    Create usercontrol to be able to loop through it

---
+++ PicView/Views/UserControls/Misc/ThumbnailOutputUC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PicView.Views.UserControls
+{
+    public partial class ThumbnailOutputUC : UserControl
+    {
+        public ThumbnailOutputUC(int i, string folderPath, string filename)
+        {
+            InitializeComponent();
+
+            OutPutString.Text = $"Thumbnail {i} output folder";
+            OutPutStringBox.Text = folderPath + @"\" + filename;
+         }
+    }
+}
