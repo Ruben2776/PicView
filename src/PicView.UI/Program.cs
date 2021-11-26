a20c11d33c511a4b2ebd1d197ed75e32798748e2
Ruben <ruben@live.dk>
2021-11-26 18:55:17 +0100

    Add global usings

--- src/PicView.UI/Program.cs
+++ src/PicView.UI/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
