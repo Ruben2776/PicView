c2154afa4f82685d763ef0b6b5b31d2d9a45d117
Ruben <ruben@live.dk>
2021-11-26 17:36:55 +0100

    Add source

---
+++ src/PicView/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PicView.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ src/PicView/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace PicView.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
