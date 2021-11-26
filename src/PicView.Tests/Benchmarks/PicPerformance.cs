1df577bb6798980479901cb7007389c2ba9157e2
Ruben <ruben@live.dk>
2021-11-26 18:38:09 +0100

    Add Tests project

---
+++ src/PicView.Tests/Benchmarks/PicPerformance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicView.Tests.Benchmarks
+{
+    internal class PicPerformance
+    {
+        // TODO create benchmarks to ascertain performance when changing picture
+    }
+}
