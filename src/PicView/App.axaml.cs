68eaf0ca5d97c3a576e6fcdba10d6e9e1c580147
Ruben <ruben@live.dk>
2021-11-27 00:36:22 +0100

    Misc updates

--- src/PicView/App.axaml.cs
+++ src/PicView/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using PicView.Styles.Themes;
 using PicView.ViewModels;
 using PicView.Views;
 
@@ -11,6 +12,13 @@ namespace PicView
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+            LoadTheme();
+        }
+
+        private void LoadTheme()
+        {
+            // TODO add more themes that can be switched
+            Styles.Add(new DarkTheme());
         }
 
         public override void OnFrameworkInitializationCompleted()
