68a6d9cb81a1dd96280052fb2322c5e2560640f7
Ruben <ruben@live.dk>
2021-12-06 21:16:32 +0100

    Create new project for non-UI code, add files, work in progress

---
+++ src/PicView.Data/IO/FileLists.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PicView.Data.IO
+{
+    public class FileLists
+    {
+        
+    }
+}
---
+++ src/PicView.Data/IO/SortFilesBy.cs
@@ -0,0 +1,13 @@
+﻿namespace PicView.Data.IO
+{
+    public enum SortFilesBy
+    {
+        Name,
+        FileSize,
+        Creationtime,
+        Extension,
+        Lastaccesstime,
+        Lastwritetime,
+        Random
+    }
+}
