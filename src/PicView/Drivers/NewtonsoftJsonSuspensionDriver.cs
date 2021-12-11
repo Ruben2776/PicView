2419317f2dec9b79652118b0b866ecb264807538
Ruben <ruben@live.dk>
2021-12-11 15:49:47 +0100

    #61 Test and update

---
+++ src/PicView/Drivers/NewtonsoftJsonSuspensionDriver.cs
@@ -0,0 +1,43 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using Newtonsoft.Json;
+using ReactiveUI;
+using Formatting = System.Xml.Formatting;
+
+namespace PicView.Drivers;
+
+public sealed class NewtonsoftJsonSuspensionDriver : ISuspensionDriver
+{
+    private readonly string _stateFilePath;
+
+    private readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+    {
+        TypeNameHandling = TypeNameHandling.All
+    };
+
+    public NewtonsoftJsonSuspensionDriver(string stateFilePath) => _stateFilePath = stateFilePath;
+
+    public IObservable<Unit> InvalidateState()
+    {
+        if (File.Exists(_stateFilePath))
+        {
+            File.Delete(_stateFilePath);
+        }
+
+        return Observable.Return(Unit.Default);
+    }
+
+    public IObservable<object> LoadState()
+    {
+        var lines = File.ReadAllText(_stateFilePath);
+        var state = JsonConvert.DeserializeObject<object>(lines, _settings);
+        return Observable.Return(state!);
+    }
+
+    public IObservable<Unit> SaveState(object state)
+    {
+        var lines = JsonConvert.SerializeObject(state, (Newtonsoft.Json.Formatting) Formatting.Indented, _settings);
+        File.WriteAllText(_stateFilePath, lines);
+        return Observable.Return(Unit.Default);
+    }
+}
---
+++ src/PicView/Settings/UserConfig.cs
@@ -0,0 +1,45 @@
+﻿namespace PicView.Settings;
+
+public class UserConfig
+{
+    public UserConfig()
+    {
+        WindowProperties windowProperties = new();
+        Theme theme = new();
+        Sorting sorting = new();
+    }
+    public class WindowProperties
+    {
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public bool AutoFit { get; set; }
+        public bool TopMost { get; set; }
+        public bool Maximized { get; set; }
+        public bool Fullscreen { get; set; }
+        public bool ShowInterface { get; set; }
+    }
+
+    public class Theme
+    {
+        public bool Dark { get; set; }
+        public bool Light { get; set; }
+    }
+
+    public class Sorting
+    {
+        public bool Name { get; set; }
+        public bool FileSize { get; set; }
+        public bool CreationTime { get; set; }
+        public bool Extension { get; set; }
+        public bool LastAccessTime { get; set; }
+        public bool LastWriteTime { get; set; }
+        public bool Random { get; set; }
+        public bool Ascending { get; set; }
+    }
+
+    public string? UserLanguage { get; set; }
+    public bool Looping { get; set; }
+    public double ZoomSpeed { get; set; }
+}
