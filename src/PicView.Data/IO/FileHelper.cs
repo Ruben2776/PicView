7b959d116b2398d20cc845be84c7e3098ae14d01
Ruben <ruben@live.dk>
2021-12-11 15:50:47 +0100

    #78 Add files

---
+++ src/PicView.Data/IO/FileHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace PicView.Data.IO;
+
+public static class FileHelper
+{
+    /// <summary>
+    /// Returns the human-readable file size for an arbitrary, 64-bit file size
+    /// The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"
+    /// </summary>
+    /// <param name="length">FileInfo.Length</param>
+    /// <returns></returns>
+    /// Credits to http://www.somacon.com/p576.php
+    public static string GetSizeReadable(long length)
+    {
+        var sign = length < 0 ? "-" : string.Empty;
+        char prefix;
+        double value;
+
+        switch (length)
+        {
+            // Gigabyte
+            case >= 0x40000000:
+                prefix = 'G';
+                value = length >> 20;
+                break;
+            // Megabyte
+            case >= 0x100000:
+                prefix = 'M';
+                value = length >> 10;
+                break;
+            // Kilobyte
+            case >= 0x400:
+                prefix = 'K';
+                value = length;
+                break;
+            // Byte
+            default:
+                return length.ToString(sign + "0 B", CultureInfo.CurrentCulture);
+        }
+        value /= 1024; // Divide by 1024 to get fractional value
+
+        return sign + value.ToString("0.## ", CultureInfo.CurrentCulture) + prefix + 'B';
+    }
+
+    public static string Shorten(string fileName, int amount)
+    {
+        if (fileName.Length < 25) { return fileName; }
+            
+        fileName = fileName[..amount];
+        fileName += "...";
+        return fileName;
+    }
+}
---
+++ src/PicView.Win32/Explorer/FileHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PicView.Win32.Explorer;
+
+public static class FileHelper
+{
+    #region Open folder and select file
+    [DllImport("shell32.dll", SetLastError = true)]
+    private static extern int SHOpenFolderAndSelectItems(IntPtr pidlFolder, uint cidl, [In, MarshalAs(UnmanagedType.LPArray)] IntPtr[] apidl, uint dwFlags);
+
+    [DllImport("shell32.dll", SetLastError = true)]
+    private static extern void SHParseDisplayName([MarshalAs(UnmanagedType.LPWStr)] string name, IntPtr bindingContext, [Out] out IntPtr pidl, uint sfgaoIn, [Out] out uint psfgaoOut);
+
+    public static void OpenFolderAndSelectItem(string folderPath, string file)
+    {
+        IntPtr nativeFolder;
+        uint psfgaoOut;
+        SHParseDisplayName(folderPath, IntPtr.Zero, out nativeFolder, 0, out psfgaoOut);
+
+        if (nativeFolder == IntPtr.Zero)
+        {
+            // Log error, can't find folder
+            return;
+        }
+
+        IntPtr nativeFile;
+        SHParseDisplayName(Path.Combine(folderPath, file), IntPtr.Zero, out nativeFile, 0, out psfgaoOut);
+
+        IntPtr[] fileArray;
+        fileArray = nativeFile == IntPtr.Zero ? Array.Empty<IntPtr>() : new[] { nativeFile };
+
+        _= SHOpenFolderAndSelectItems(nativeFolder, (uint)fileArray.Length, fileArray, 0);
+
+        Marshal.FreeCoTaskMem(nativeFolder);
+        if (nativeFile != IntPtr.Zero)
+        {
+            Marshal.FreeCoTaskMem(nativeFile);
+        }
+    }
+    #endregion
+}
