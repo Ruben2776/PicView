1a7a6790823970430693dbd36427962c027e869a
Ruben <ruben@live.dk>
2023-06-12 09:16:36 +0200

    Update logo colors

--- src/PicView/ConfigureSettings/ConfigColors.cs
+++ src/PicView/ConfigureSettings/ConfigColors.cs
@@ -239,7 +239,7 @@ internal static class ConfigColors
 
     internal static Color GetSecondaryAccentColor => Settings.Default.ColorTheme switch
     {
-        0 => Color.FromRgb(234, 182, 60), // Blue
+        0 => Color.FromRgb(255, 240, 90), // Blue
         2 => Color.FromRgb(255, 237, 38), // Pink
         3 => Color.FromRgb(248, 175, 60), // Orange
         4 => Color.FromRgb(209, 237, 93), // Green
@@ -247,8 +247,8 @@ internal static class ConfigColors
         6 => Color.FromRgb(254, 172, 150), // Teal
         7 => Color.FromRgb(228, 209, 17), // Aqua
         8 => Color.FromRgb(255, 253, 42), // Golden
-        9 => Color.FromRgb(209, 237, 93), // Purple
-        10 => Color.FromRgb(180, 246, 88), // Cyan
+        9 => Color.FromRgb(229, 237, 93), // Purple
+        10 => Color.FromRgb(255, 253, 66), // Cyan
         11 => Color.FromRgb(255, 237, 38), // Magenta
         12 => Color.FromRgb(255, 253, 42), // Lime
         _ => throw new ArgumentOutOfRangeException(nameof(Settings.Default.ColorTheme)),
