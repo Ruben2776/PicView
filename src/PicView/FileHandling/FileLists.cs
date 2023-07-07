145ad73bdd5ac4821a472b71ba238efea43fd1c8
Ruben <ruben@live.dk>
2023-07-07 22:01:28 +0200

    Fix crash; skip loading of folders with unauthorized access

--- src/PicView/FileHandling/FileLists.cs
+++ src/PicView/FileHandling/FileLists.cs
@@ -1,8 +1,12 @@
 ﻿using PicView.ChangeImage;
 using PicView.Properties;
 using PicView.SystemIntegration;
+using System.Diagnostics;
 using System.IO;
+using PicView.UILogic;
 using static PicView.FileHandling.ArchiveExtraction;
+using Microsoft.VisualBasic.FileIO;
+using SearchOption = System.IO.SearchOption;
 
 namespace PicView.FileHandling;
 
@@ -86,30 +90,44 @@ internal static class FileLists
         var directory = isDirectory ? fileInfo.FullName : fileInfo.DirectoryName;
         if (directory is null) return null;
 
+        IEnumerable<string> files;
+        string[] enumerable;
         // Check if the subdirectories are to be included in the search
-        var searchOption = Settings.Default.IncludeSubDirectories && string.IsNullOrWhiteSpace(TempZipFile)
-            ? SearchOption.AllDirectories
-            : SearchOption.TopDirectoryOnly;
-
-        // Get the list of files in the directory
-        var items = Directory.EnumerateFiles(directory, "*.*", searchOption);
-        var enumerable = items as string[] ?? items.ToArray();
-        if (searchOption == SearchOption.AllDirectories)
+        var recurseSubdirectories = Settings.Default.IncludeSubDirectories && string.IsNullOrWhiteSpace(TempZipFile);
+        try
         {
-            enumerable.AsParallel();
+            // Get the list of files in the directory
+            files = Directory.EnumerateFiles(directory, "*.*", new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = recurseSubdirectories,
+            });
+            enumerable = files as string[] ?? files.ToArray();
+            if (recurseSubdirectories)
+            {
+                enumerable.AsParallel();
+            }
+        }
+        catch (Exception exception)
+        {
+#if DEBUG
+            Trace.WriteLine($"{nameof(FileList)} {fileInfo.Name} exception:\n{exception.Message}");
+#endif
+            files = Directory.EnumerateFiles(directory, "*.*", SearchOption.TopDirectoryOnly);
+            enumerable = files as string[] ?? files.ToArray();
         }
 
         // Filter out files with invalid extensions
         var extensions = SupportedFiles.FileExtensions;
         bool IsExtensionValid(string f) => extensions.Contains(Path.GetExtension(f), StringComparer.OrdinalIgnoreCase);
-        items = enumerable.Where((Func<string, bool>)IsExtensionValid);
+        files = enumerable.Where((Func<string, bool>)IsExtensionValid);
 
         // Sort the file names based on the specified sorting method
         switch (sortFilesBy)
         {
             default:
             case SortFilesBy.Name: // Alphanumeric sort
-                var list = items.ToList();
+                var list = files.ToList();
                 if (Settings.Default.Ascending)
                 {
                     list.Sort(NativeMethods.StrCmpLogicalW);
@@ -121,33 +139,33 @@ internal static class FileLists
                 return list;
 
             case SortFilesBy.FileSize: // Sort by file size
-                var fileInfoList = items.Select(f => new FileInfo(f)).ToList();
+                var fileInfoList = files.Select(f => new FileInfo(f)).ToList();
                 var sortedBySize = Settings.Default.Ascending ?
                     fileInfoList.OrderBy(f => f.Length) : fileInfoList.OrderByDescending(f => f.Length);
                 return sortedBySize.Select(f => f.FullName).ToList();
 
             case SortFilesBy.Extension: // Sort by file extension
                 var sortedByExtension = Settings.Default.Ascending ?
-                    items.OrderBy(Path.GetExtension) : items.OrderByDescending(Path.GetExtension);
+                    files.OrderBy(Path.GetExtension) : files.OrderByDescending(Path.GetExtension);
                 return sortedByExtension.ToList();
 
             case SortFilesBy.CreationTime: // Sort by file creation time
                 var sortedByCreationTime = Settings.Default.Ascending ?
-                    items.OrderBy(f => new FileInfo(f).CreationTime) : items.OrderByDescending(f => new FileInfo(f).CreationTime);
+                    files.OrderBy(f => new FileInfo(f).CreationTime) : files.OrderByDescending(f => new FileInfo(f).CreationTime);
                 return sortedByCreationTime.ToList();
 
             case SortFilesBy.LastAccessTime: // Sort by file last access time
                 var sortedByLastAccessTime = Settings.Default.Ascending ?
-                    items.OrderBy(f => new FileInfo(f).LastAccessTime) : items.OrderByDescending(f => new FileInfo(f).LastAccessTime);
+                    files.OrderBy(f => new FileInfo(f).LastAccessTime) : files.OrderByDescending(f => new FileInfo(f).LastAccessTime);
                 return sortedByLastAccessTime.ToList();
 
             case SortFilesBy.LastWriteTime: // Sort by file last write time
                 var sortedByLastWriteTime = Settings.Default.Ascending ?
-                    items.OrderBy(f => new FileInfo(f).LastWriteTime) : items.OrderByDescending(f => new FileInfo(f).LastWriteTime);
+                    files.OrderBy(f => new FileInfo(f).LastWriteTime) : files.OrderByDescending(f => new FileInfo(f).LastWriteTime);
                 return sortedByLastWriteTime.ToList();
 
             case SortFilesBy.Random: // Sort files randomly
-                return items.OrderBy(f => Guid.NewGuid()).ToList();
+                return files.OrderBy(f => Guid.NewGuid()).ToList();
         }
     }
 
