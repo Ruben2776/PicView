bb466ed7d69e3bf2d1a5705b97defd34ab8eba25
Ruben <ruben@live.dk>
2023-12-23 20:14:30 +0100

    Update json settings logic

--- src/PicView.Core/Config/AppSettings.cs
+++ src/PicView.Core/Config/AppSettings.cs
@@ -1,84 +1,78 @@
-﻿namespace PicView.Core.Config
+﻿namespace PicView.Core.Config;
+
+public class AppSettings
 {
-    public class AppSettings
-    {
-        public WindowProperties WindowProperties { get; set; }
-        public UIProperties UIProperties { get; set; }
-        public Theme Theme { get; set; }
-        public Gallery Gallery { get; set; }
-        public ImageScaling ImageScaling { get; set; }
-        public Sorting Sorting { get; set; }
-        public Zoom Zoom { get; set; }
-    }
+    public int Version { get; set; } = 1;
+    public WindowProperties? WindowProperties { get; set; }
+    public UIProperties? UIProperties { get; set; }
+    public Theme? Theme { get; set; }
+    public Gallery? Gallery { get; set; }
+    public ImageScaling? ImageScaling { get; set; }
+    public Sorting? Sorting { get; set; }
+    public Zoom? Zoom { get; set; }
+}
 
-    public class WindowProperties
-    {
-        public double Top { get; set; }
-        public double Left { get; set; }
-        public double Width { get; set; }
-        public double Height { get; set; }
-        public bool AutoFit { get; set; }
-        public bool TopMost { get; set; }
-        public bool Maximized { get; set; }
-        public bool Fullscreen { get; set; }
-        public bool KeepCentered { get; set; }
-    }
+public class WindowProperties
+{
+    public double Top { get; set; } = 300;
+    public double Left { get; set; } = 500;
+    public double Width { get; set; } = 750;
+    public double Height { get; set; } = 1024;
+    public bool AutoFit { get; set; } = false;
+    public bool TopMost { get; set; } = false;
+    public bool Maximized { get; set; } = false;
+    public bool Fullscreen { get; set; } = false;
+    public bool KeepCentered { get; set; } = false;
+}
 
-    public class UIProperties
-    {
-        public string UserLanguage { get; set; }
-        public bool ShowInterface { get; set; }
-        public bool ShowAltInterfaceButtons { get; set; }
-        public bool ShowFileSavingDialog { get; set; }
-        public bool ShowBottomNavBar { get; set; }
-        public bool IsTaskbarProgressEnabled { get; set; }
-        public double NavSpeed { get; set; }
-        public bool Looping { get; set; }
-        public int BgColorChoice { get; set; }
-        public double SlideShowTimer { get; set; }
-    }
+public class UIProperties
+{
+    public string UserLanguage { get; set; } = "en";
+    public bool ShowInterface { get; set; } = true;
+    public bool ShowAltInterfaceButtons { get; set; } = true;
+    public bool ShowFileSavingDialog { get; set; } = true;
+    public bool ShowBottomNavBar { get; set; } = true;
+    public bool IsTaskbarProgressEnabled { get; set; } = true;
+    public double NavSpeed { get; set; } = 0.3;
+    public bool Looping { get; set; } = false;
+    public int BgColorChoice { get; set; } = 0;
+    public double SlideShowTimer { get; set; } = 5000;
+}
 
-    public class Theme
-    {
-        public bool Dark { get; set; }
-        public bool Light { get; set; }
-        public int ColorTheme { get; set; }
-    }
+public class Theme
+{
+    public bool Dark { get; set; } = true;
+    public bool Light { get; set; } = false;
+    public int ColorTheme { get; set; } = 3;
+}
 
-    public class Gallery
-    {
-        public bool IsBottomGalleryShown { get; set; }
-        public bool ShowBottomGalleryInHiddenUI { get; set; }
-        public double BottomGalleryItemSize { get; set; }
-        public double ExpandedGalleryItemSize { get; set; }
-    }
+public class Gallery
+{
+    public bool IsBottomGalleryShown { get; set; } = false;
+    public bool ShowBottomGalleryInHiddenUI { get; set; } = false;
+    public double BottomGalleryItemSize { get; set; } = 37;
+    public double ExpandedGalleryItemSize { get; set; } = 23;
+}
 
-    public class ImageScaling
-    {
-        public bool StretchImage { get; set; }
-        public bool IsScalingSetToNearestNeighbor { get; set; }
-    }
+public class ImageScaling
+{
+    public bool StretchImage { get; set; } = false;
+    public bool IsScalingSetToNearestNeighbor { get; set; } = false;
+}
 
-    public class Zoom
-    {
-        public double ZoomSpeed { get; set; }
-        public bool AvoidZoomingOut { get; set; }
-        public bool CtrlZoom { get; set; }
-        public bool HorizontalReverseScroll { get; set; }
-        public bool ScrollEnabled { get; set; }
-    }
+public class Zoom
+{
+    public double ZoomSpeed { get; set; } = 0.3;
+    public bool AvoidZoomingOut { get; set; } = false;
+    public bool CtrlZoom { get; set; } = false;
+    public bool HorizontalReverseScroll { get; set; } = true;
+    public bool ScrollEnabled { get; set; } = false;
+}
 
-    public class Sorting
-    {
-        public bool Name { get; set; }
-        public bool FileSize { get; set; }
-        public bool Creationtime { get; set; }
-        public bool Extension { get; set; }
-        public bool Lastaccesstime { get; set; }
-        public bool Lastwritetime { get; set; }
-        public bool Random { get; set; }
-        public bool Ascending { get; set; }
-        public int SortPreference { get; set; }
-        public bool IncludeSubDirectories { get; set; }
-    }
+public class Sorting
+{
+    public bool Name { get; set; } = true;
+    public bool Ascending { get; set; } = true;
+    public int SortPreference { get; set; } = 0;
+    public bool IncludeSubDirectories { get; set; } = false;
 }
--- src/PicView.Core/Config/SettingsHelper.cs
+++ src/PicView.Core/Config/SettingsHelper.cs
@@ -1,28 +1,37 @@
 ﻿using System.Diagnostics;
-using System.Text;
 using System.Text.Json;
 
 namespace PicView.Core.Config;
 
 public static class SettingsHelper
 {
-    public static AppSettings? Settings { get; set; }
+    private const int CurrentSettingsVersion = 1;
+
+    public static AppSettings? Settings { get; private set; }
     private static JsonSerializerOptions? _jsonSerializerOptions;
 
     public static async Task LoadSettingsAsync()
     {
         try
         {
-            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config/AppSettings.json");
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config/UserSettings.json");
             if (File.Exists(path))
             {
-                // Read JSON File with UTF-8 encoding
                 var jsonString = await File.ReadAllTextAsync(path).ConfigureAwait(false);
-                Settings = JsonSerializer.Deserialize<AppSettings>(jsonString);
+                Settings = await UpgradeSettings(JsonSerializer.Deserialize<AppSettings>(jsonString)).ConfigureAwait(false);
             }
             else
             {
-                // TODO recreate a default json settings file
+                Settings = new AppSettings
+                {
+                    UIProperties = new UIProperties(),
+                    Gallery = new Gallery(),
+                    ImageScaling = new ImageScaling(),
+                    Sorting = new Sorting(),
+                    Theme = new Theme(),
+                    WindowProperties = new WindowProperties(),
+                    Zoom = new Zoom()
+                };
             }
         }
         catch (Exception ex)
@@ -36,13 +45,68 @@ public static class SettingsHelper
         try
         {
             _jsonSerializerOptions ??= new JsonSerializerOptions { WriteIndented = true };
-            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config/AppSettings.json");
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config/UserSettings.json");
             var updatedJson = JsonSerializer.Serialize(Settings, _jsonSerializerOptions);
             await File.WriteAllTextAsync(path, updatedJson).ConfigureAwait(false);
         }
         catch (Exception ex)
         {
             Trace.WriteLine($"{nameof(SaveSettingsAsync)} error saving settings:\n {ex.Message}");
+        }
+    }
+
+    private static async Task<AppSettings> UpgradeSettings(AppSettings settings)
+    {
+        if (settings.Version >= CurrentSettingsVersion)
+        {
+            return settings;
+        }
+
+        await SynchronizeSettings(settings).ConfigureAwait(false);
+
+        settings.Version = CurrentSettingsVersion;
+
+        return settings;
+    }
+
+    private static async Task SynchronizeSettings(AppSettings settings)
+    {
+        try
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config/UserSettings.json");
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            var jsonString = await File.ReadAllTextAsync(path).ConfigureAwait(false);
+            var existingSettings = JsonSerializer.Deserialize<AppSettings>(jsonString);
+
+            if (existingSettings == null)
+            {
+                return;
+            }
+
+            var properties = typeof(AppSettings).GetProperties();
+            foreach (var property in properties)
+            {
+                // Check if the property exists in the existing JSON
+                var jsonProperty = existingSettings.GetType().GetProperty(property.Name);
+                if (jsonProperty == null)
+                {
+                    // Property exists in C# class but not in JSON, initialize it
+                    property.SetValue(existingSettings, property.GetValue(settings));
+                }
+            }
+
+            // Save the synchronized settings back to the JSON file
+            _jsonSerializerOptions ??= new JsonSerializerOptions { WriteIndented = true };
+            var updatedJson = JsonSerializer.Serialize(existingSettings, _jsonSerializerOptions);
+            await File.WriteAllTextAsync(path, updatedJson).ConfigureAwait(false);
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine($"{nameof(SynchronizeSettings)} error synchronizing settings:\n {ex.Message}");
         }
     }
 }
