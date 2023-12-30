844fa6690e8c432025b927ed90aab306a89500ac
Ruben <ruben@live.dk>
2023-12-30 22:38:17 +0100

    Refactor

--- src/PicView.WPF/UILogic/TransformImage/Rotation.cs
+++ src/PicView.WPF/UILogic/TransformImage/Rotation.cs
@@ -5,6 +5,7 @@ using PicView.WPF.UILogic.Sizing;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using PicView.Core.ImageTransformations;
 
 namespace PicView.WPF.UILogic.TransformImage;
 
@@ -56,13 +57,13 @@ internal static class Rotation
         }
         else
         {
-            if (IsValidRotation(RotationAngle))
+            if (RotationHelper.IsValidRotation(RotationAngle))
             {
                 Rotate(up);
             }
             else
             {
-                Rotate(NextRotationAngle(RotationAngle, up), true);
+                Rotate(RotationHelper.NextRotationAngle(RotationAngle, up), true);
             }
         }
     }
@@ -180,46 +181,5 @@ internal static class Rotation
         ConfigureWindows.GetMainWindow.MainImage.LayoutTransform = tg;
 
         UpdateUIValues.ChangeFlipButton();
-    }
-
-    /// <summary>
-    /// This method checks if the provided rotation angle is valid or not.
-    /// A rotation angle is considered valid if it is a multiple of 90 degrees and lies within the range of 0 to 360 degrees (inclusive).
-    /// If the provided angle is not valid, the method returns false. Otherwise, it returns true.
-    /// </summary>
-    /// <param name="rotationAngle">A double value representing the rotation angle to be checked.</param>
-    /// <returns>A bool value representing whether the provided rotation angle is valid or not.</returns>
-    private static bool IsValidRotation(double rotationAngle)
-    {
-        rotationAngle %= 360;
-        return rotationAngle is 0 or 90 or 180 or 270;
-    }
-
-    /// <summary>
-    /// This method returns the next rotation angle based on the current rotation angle and the direction of rotation.
-    /// </summary>
-    /// <param name="currentDegrees">The current rotation angle in degrees.</param>
-    /// <param name="clockWise">A boolean value indicating the direction of rotation. If true, the rotation is clockwise; otherwise, it is counterclockwise.</param>
-    /// <returns>The next rotation angle in degrees.</returns>
-    private static int NextRotationAngle(double currentDegrees, bool clockWise)
-    {
-        if (clockWise)
-        {
-            return currentDegrees switch
-            {
-                > 0 and < 90 => 90,
-                > 90 and < 180 => 180,
-                > 180 and < 270 => 270,
-                _ => 0
-            };
-        }
-
-        return currentDegrees switch
-        {
-            > 0 and < 90 => 0,
-            > 90 and < 180 => 90,
-            > 180 and < 270 => 180,
-            _ => 270
-        };
     }
 }
