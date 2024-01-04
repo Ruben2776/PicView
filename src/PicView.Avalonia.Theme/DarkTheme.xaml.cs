f6909268b1f28c8d93915909ace5b5092d2c7bf6
Ruben <ruben@live.dk>
2024-01-04 21:28:25 +0100

    Avalonia style updates

---
+++ src/PicView.Avalonia.Theme/DarkTheme.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace PicView.Avalonia.Theme;
+
+public class DarkTheme : Styles
+{
+    public DarkTheme(IServiceProvider? sp = null)
+    {
+        AvaloniaXamlLoader.Load(sp, this);
+    }
+}
--- src/PicView.Avalonia.Win32/Views/MainWindow.axaml.cs
+++ src/PicView.Avalonia.Win32/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Media;
 
 namespace PicView.Avalonia.Win32.Views;
 
