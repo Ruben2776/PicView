0662dca68c899b9f8cbe3eecbea5b66c9fb3d965
Ruben <ruben@live.dk>
2024-03-17 14:54:08 +0100

    Avalonia updates

---
+++ src/PicView.Avalonia/CustomControls/CopyButton.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace PicView.Avalonia.CustomControls;
+
+public class CopyButton : Button
+{
+    public static readonly AvaloniaProperty<string> CopyTextProperty =
+        AvaloniaProperty.Register<CopyButton, string>(nameof(CopyText));
+
+    public string CopyText
+    {
+        get => (string)GetValue(CopyTextProperty);
+        set => SetValue(CopyTextProperty, value);
+    }
+
+    public CopyButton()
+    {
+        Click += CopyButton_OnClick;
+    }
+
+    private async void CopyButton_OnClick(object? sender, RoutedEventArgs args)
+    {
+        if (string.IsNullOrWhiteSpace(CopyText))
+        {
+            return;
+        }
+        var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
+        var dataObject = new DataObject();
+        dataObject.Set(DataFormats.Text, CopyText);
+        await clipboard.SetDataObjectAsync(dataObject);
+    }
+}
