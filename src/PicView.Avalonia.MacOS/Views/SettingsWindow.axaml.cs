b68914dd51f5c0f99e66fbdb54c12bd3d5180332
Ruben <ruben@live.dk>
2024-03-19 22:20:32 +0100

    Avalonia updates

--- src/PicView.Avalonia.MacOS/Views/SettingsWindow.axaml.cs
+++ src/PicView.Avalonia.MacOS/Views/SettingsWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using PicView.Core.Localization;
 
 namespace PicView.Avalonia.MacOS.Views;
 
@@ -7,5 +10,19 @@ public partial class SettingsWindow : Window
     public SettingsWindow()
     {
         InitializeComponent();
+        Loaded += (sender, e) =>
+        {
+            MinWidth = MaxWidth = Width;
+            Height = 500;
+            Title = TranslationHelper.GetTranslation("Settings") + " - PicView";
+        };
+    }
+
+    private void MoveWindow(object? sender, PointerPressedEventArgs e)
+    {
+        if (VisualRoot is null) { return; }
+
+        var hostWindow = (Window)VisualRoot;
+        hostWindow?.BeginMoveDrag(e);
     }
 }
