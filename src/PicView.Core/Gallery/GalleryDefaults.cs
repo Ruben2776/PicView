5acb020120ce854c6c73824f3378798ef3e039ed
Ruben <ruben@live.dk>
2024-08-13 15:26:37 +0200

    Adjust defaults

--- src/PicView.Core/Gallery/GalleryDefaults.cs
+++ src/PicView.Core/Gallery/GalleryDefaults.cs
@@ -2,12 +2,12 @@
 
 public static class GalleryDefaults
 {
+    public const int DefaultFullGalleryHeight = 150;
     public const int DefaultBottomGalleryHeight = 53;
-    public const int DefaultFullGalleryHeight = 120;
     
     public const int MaxFullGalleryItemHeight = 350;
     public const int MinFullGalleryItemHeight = 45;
     
-    public const int MinBottomGalleryItemHeight = 25;
     public const int MaxBottomGalleryItemHeight = 125;
+    public const int MinBottomGalleryItemHeight = 25;
 }
