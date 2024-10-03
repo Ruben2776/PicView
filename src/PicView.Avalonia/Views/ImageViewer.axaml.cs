e5594a4d8cfb3578aa8b1bc2277bd8fd6c44fbc5
Ruben <ruben@live.dk>
2024-10-03 21:14:47 +0200

    Fix adjusting zooming speed #167

--- src/PicView.Avalonia/Views/ImageViewer.axaml.cs
+++ src/PicView.Avalonia/Views/ImageViewer.axaml.cs
@@ -231,25 +231,6 @@ public partial class ImageViewer : UserControl
         var currentZoom = _scaleTransform.ScaleX;
         var zoomSpeed = SettingsHelper.Settings.Zoom.ZoomSpeed;
 
-        switch (currentZoom)
-        {
-            // Increase speed based on the current zoom level
-            case > 14 when isZoomIn:
-                return;
-
-            case > 4:
-                zoomSpeed += 1.5;
-                break;
-
-            case > 3.2:
-                zoomSpeed += 1;
-                break;
-
-            case > 1.6:
-                zoomSpeed += 0.5;
-                break;
-        }
-
         if (!isZoomIn)
         {
             zoomSpeed = -zoomSpeed;
