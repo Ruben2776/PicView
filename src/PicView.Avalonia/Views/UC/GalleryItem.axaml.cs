c23ea04592f62e0d8953e1eacd935bc5d05694b1
Ruben <ruben@live.dk>
2024-10-05 01:17:27 +0200

    Highlight gallery context menu when right-clicking

--- src/PicView.Avalonia/Views/UC/GalleryItem.axaml.cs
+++ src/PicView.Avalonia/Views/UC/GalleryItem.axaml.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Exception = System.Exception;
 
 namespace PicView.Avalonia.Views.UC
 {
@@ -7,6 +10,31 @@ namespace PicView.Avalonia.Views.UC
         public GalleryItem()
         {
             InitializeComponent();
+            GalleryContextMenu.Opened += delegate
+            {
+                if (!Application.Current.TryGetResource("SecondaryAccentColor", Application.Current.RequestedThemeVariant, out var color))
+                {
+                    return;
+                }
+
+                try
+                {
+                    var secondaryAccentBrush = (SolidColorBrush)color;
+                    ImageBorder.BorderBrush = secondaryAccentBrush;
+                }
+#if DEBUG
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+#else
+        catch (Exception){}
+#endif
+            };
+            GalleryContextMenu.Closed += delegate
+            {
+                ImageBorder.BorderBrush = Brushes.Transparent;
+            };
         }
     }
 }
