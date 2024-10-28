9bb1ccc234a8b4a62abff7695776cd00beea92cd
Ruben <ruben@live.dk>
2024-10-28 16:00:44 +0100

    Update build logic

---
+++ Build/Get-VersionInfo.ps1
@@ -0,0 +1,16 @@
+# Get-VersionInfo.ps1
+
+# Load Directory.Build.props as an XML
+[xml]$xml = Get-Content "$PSScriptRoot/../Directory.Build.props"
+
+# Extract VersionPrefix, VersionSuffix, and FileVersion
+$versionPrefix = $xml.Project.PropertyGroup.VersionPrefix
+$versionSuffix = $xml.Project.PropertyGroup.VersionSuffix
+$fileVersion = $xml.Project.PropertyGroup.FileVersion
+
+# Combine VersionPrefix and VersionSuffix
+$fullVersion = "$versionPrefix-$versionSuffix"
+
+# Output the results for GitHub Actions
+Write-Output "::set-output name=version::$fullVersion"
+Write-Output "::set-output name=file-version::$fileVersion"
