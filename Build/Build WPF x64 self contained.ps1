3527d1f0b0ad09e3e6e508ba6677ef2183856efe
Ruben <ruben@live.dk>
2024-11-23 14:09:11 +0100

    Update build logic

--- Build/Build WPF x64 self contained.ps1
+++ Build/Build WPF x64 self contained.ps1
@@ -26,8 +26,39 @@ if ($packageNodes) {
 # Define the project path for the actual build target
 $projectPath = Join-Path -Path $PSScriptRoot -ChildPath "..\src\PicView.WPF\PicView.WPF.csproj"
 
+$publishPath = Join-Path -Path $PSScriptRoot -ChildPath "PicView-x64-self-contained"
+
 # Run dotnet publish for the project
-dotnet publish $projectPath --runtime win-x64 --self-contained true --configuration Release --output "$PSScriptRoot/PicView-x64-self-contained" /p:PublishReadyToRun=true
+dotnet publish $projectPath --runtime win-x64 --self-contained true --configuration Release --output $publishPath /p:PublishReadyToRun=true
+
+#rm "$publishPath/System.Windows.Forms*"
+rm "$publishPath/Microsoft.VisualBasic.Forms.dll"
+rm "$publishPath/Microsoft.Web.WebView2.WinForms.dll"
+rm "$publishPath/PicView.pdb"
+rm "$publishPath/PicView.Core.pdb"
+rm "$publishPath/PicView.dll.config"
+rm "$publishPath/createdump.exe" 
+rm "$publishPath/XamlAnimatedGif.pdb" 
+rm -r "$publishPath/ar"
+rm -r "$publishPath/cs"
+rm -r "$publishPath/da"
+rm -r "$publishPath/de"
+rm -r "$publishPath/es"
+rm -r "$publishPath/fr"
+rm -r "$publishPath/it"
+rm -r "$publishPath/ja*"
+rm -r "$publishPath/ko"
+rm -r "$publishPath/lv"
+rm -r "$publishPath/nl"
+rm -r "$publishPath/pt*"
+rm -r "$publishPath/pl"
+rm -r "$publishPath/ru"
+rm -r "$publishPath/sk"
+rm -r "$publishPath/sv"
+rm -r "$publishPath/th"
+rm -r "$publishPath/tr"
+rm -r "$publishPath/zh*"
+
 
 #Remove unintended space
 if (-not [string]::IsNullOrEmpty($outputPath)) {
