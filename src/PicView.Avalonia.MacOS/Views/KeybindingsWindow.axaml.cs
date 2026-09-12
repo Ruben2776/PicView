2acbe447e02400ec06a8bd8408e10bf4a58832e5
Ruben <Ruben2776@users.noreply.github.com>
2026-09-12 02:18:55 +0200

    Remove `GlassTheme`-specific handling and unused theme-related logic across views to simplify initialization and improve maintainability.

--- src/PicView.Avalonia.MacOS/Views/KeybindingsWindow.axaml.cs
+++ src/PicView.Avalonia.MacOS/Views/KeybindingsWindow.axaml.cs
@@ -8,19 +8,9 @@ namespace PicView.Avalonia.MacOS.Views;
 
 public partial class KeybindingsWindow : GenericWindow
 {
-
     public KeybindingsWindow(KeybindingWindowConfig config)
     {
         InitializeComponent();
-        if (Settings.Theme.GlassTheme)
-        {
-            WindowBorder.Background = Brushes.Transparent;
-            XKeybindingsView.Background = Brushes.Transparent;
-        }
-        else if (!Settings.Theme.Dark)
-        {
-            XKeybindingsView.Background = UIHelper.GetMenuBackgroundColor();
-        }
         GenericWindowHelper.GenericWindowInitialize(this, TranslationManager.Translation.ApplicationShortcuts, true, config.WindowProperties);
         MinWidth = MaxWidth = 618;
     }
